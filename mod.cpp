5c2c2c8ec9ecd2815201f1650ebc29601f25ffc2
LightningFrisk <bmxcaleb@gmail.com>
2022-03-17 16:54:28 -0500

    v0.0 start

---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "Gundam 00 Assets and Supplements";
+author = "Trip & Shawgrass";
+picture = "logo_ca.paa";
+logo = "logo_ca.paa";
+overview = "Gundam 00 Assets";
+description = "Gundam 00 Assets";
+tooltip = "Gundam 00 Assets";
+tooltipOwned = "Gundam 00 Assets";
+overviewPicture = "logo_ca.paa";
+overviewText = "Gundam 00 Assets";
